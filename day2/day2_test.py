5fe58d6cca5913105bd0c499331e2a4c2f5a13ef
manu <manu@manu-pc>
2019-12-03 03:05:38 +0100

    simple solution

--- day2/day2_test.py
+++ day2/day2_test.py
@@ -1,7 +1,30 @@
 import unittest
+from day2 import start2
 
 
-class day2_test(unittest.TestCase):
+class TestIntcode(unittest.TestCase):
 
-    def test_test1(self):
-        
+    def test_case_1(self):
+        intcodes = [1,0,0,0,99]
+        start(intcodes)
+        self.assertEqual(intcodes,[2,0,0,0,99])
+
+    def test_case_2(self):
+        intcodes = [2,3,0,3,99]
+        start(intcodes)
+        self.assertEqual(intcodes,[2,3,0,6,99])
+    
+    def test_case_3(self):
+        intcodes = [2,4,4,5,99,0]
+        start(intcodes)
+        self.assertEqual(intcodes,[2,4,4,5,99,9801])
+
+    def test_case_4(self):
+        intcodes = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,10,23,27,2,27,13,31,1,10,31,35,1,35,9,39,2,39,13,43,1,43,5,47,1,47,6,51,2,6,51,55,1,5,55,59,2,9,59,63,2,6,63,67,1,13,67,71,1,9,71,75,2,13,75,79,1,79,10,83,2,83,9,87,1,5,87,91,2,91,6,95,2,13,95,99,1,99,5,103,1,103,2,107,1,107,10,0,99,2,0,14,0]
+        start2(intcodes)
+        self.assertEqual(intcodes,[30,1,1,4,2,5,6,0,99])
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
