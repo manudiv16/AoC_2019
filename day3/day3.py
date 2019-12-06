0bd72316296093275efc35815e6b8154a31dc396
manu <manu@manu-pc>
2019-12-06 02:21:07 +0100

    more recursive

--- day3/day3.py
+++ day3/day3.py
@@ -5,47 +5,40 @@ coordenate = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
 ordenades = {'L': 0, 'R': 0, 'U': 1, 'D': -1}
 
 
-def get_points(wire_one):
+def get_points(A):
     x = 0
     y = 0
     length = 0
     result = {}
-    for movement in wire_one:
-        d = movement[0]
-        n = int(movement[1:])
-        assert d in ['L', 'R', 'U', 'D']
-        for _ in range(n):
-            x += coordenate[d]
-            y += ordenades[d]
+    for movement in A:
+        assert movement[0] in ['L', 'R', 'U', 'D']
+        for _ in range(int(movement[1:])):
+            x += coordenate[movement[0]]
+            y += ordenades[movement[0]]
             length += 1
             if (x, y) not in result:
                 result[(x, y)] = length
-    return result
+            
+    return result 
 
 
 def read():
     wire_one, wire_two, _ = open("input.txt").read().split("\n")
-    return [x.split(",") for x in [wire_one, wire_two]]
-
-def part1(points_wire_two,points_wire_one,path):
+    return [x.split(",") for x in [wire_one, wire_two]] 
+            
+def part1((points_wire_two,points_wire_one),path):
+    print (min(map(lambda (x,y):abs(x)+abs(y) ,path)))
     
-    result = min([abs(x)+abs(y)for (x, y) in path])
-    print (result)
-
-def part2(points_wire_one,points_wire_two,path):
-    result = min([points_wire_one[p]+points_wire_two[p] for p in path])
-    print (result)
-
+def part2((points_wire_one,points_wire_two),path):
+    print (min([points_wire_one[p]+points_wire_two[p] for p in path]))
+    
 
 def main():
 
     wire_one, wire_two = read()
-    points_wire_one = get_points(wire_one)
-    points_wire_two = get_points(wire_two)
-    path = set(points_wire_one.keys()) & set(points_wire_two.keys())
-    part1(points_wire_one,points_wire_two,path)
-    part2(points_wire_one,points_wire_two,path)
+    poin = map(get_points,[wire_one,wire_two])
+    path = lambda (x,y) : set(x.keys()) & set(y.keys())
+    part1(poin,path(poin))
+    part2(poin,path(poin))
     
-
-
 main()
