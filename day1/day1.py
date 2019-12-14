f1992f0885cc595c576fe2bb2b2cbc22e36261c1
manudiv16 <manudiv16@gmail.com>
2019-12-15 00:23:46 +0100

    python 2 syntax to python 3

--- day1/day1.py
+++ day1/day1.py
@@ -2,11 +2,9 @@
 calcule_fuel = lambda x: (x/3)-2
 sum_fuel = lambda x :sum(calcule_fuel_two(x))
 
-def part_one(doc):
-   print("part 1: "+ str(sum(map(calcule_fuel, doc))))
 
 def part_two(doc):
-    print("part 2: "+ str(sum(map(sum_fuel,doc))))
+    return str(sum(map(sum_fuel,doc)))
 
 def calcule_fuel_two(mass):
     if calcule_fuel(mass) > 0:
@@ -14,14 +12,14 @@ def calcule_fuel_two(mass):
     else :
         return []
 
-def raead():
+def part_one(doc):
+    return str(sum(map(calcule_fuel, doc)))
+
+def read():
     document =  open("input").read().split("\n")
     return map(int,document)
 
     
-doc = raead()
-part_one(doc)
-part_two(doc)
-
-
-     
+doc = read()
+print("part 1: "+ part_one(doc))
+print("part 2: "+ part_two(doc))
--- day3/day3.py
+++ day3/day3.py
@@ -11,7 +11,6 @@ def get_points(A):
     length = 0
     result = {}
     for movement in A:
-        assert movement[0] in ['L', 'R', 'U', 'D']
         for _ in range(int(movement[1:])):
             x += coordenate[movement[0]]
             y += ordenades[movement[0]]
@@ -21,26 +20,33 @@ def get_points(A):
 
     return result
 
+        
 
 def read():
     wire_one, wire_two, _ = open("input.txt").read().split("\n")
     return [x.split(",") for x in [wire_one, wire_two]]
 
+def absolute(path):
+    x,y = path
+    return abs(x)+abs(y)
 
-def part1( path):
-    print(min(map(lambda (x, y): abs(x)+abs(y), path)))
+def part1(path):
+    print(min(map(absolute, path)))
 
 
 
-def part2((points_wire_one, points_wire_two), path):
-    print(min([points_wire_one[l]+points_wire_two[l] for l in path]))
+def part2(poin, path):
+    points_wire_one,points_wire_two = poin
+    print(min(map(lambda x :points_wire_one[x]+points_wire_two[x] , path )))
 
 
 def main():
 
     wire_one, wire_two = read()
     poin = map(get_points, [wire_one, wire_two])
-    def path((x, y)): return set(x.keys()) & set(y.keys())
+    def path(poin):
+        x,y = poin
+        return set(x.keys()) & set(y.keys())
     part1(path(poin))
     part2(poin, path(poin))
 
